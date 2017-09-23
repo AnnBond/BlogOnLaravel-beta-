37104a96c50d920f5f13096fd57d5c1299262d22
AnnBond <annbondar4uk@gmail.com>
2017-09-24 01:24:32 +0300

    add methods for comments

---
+++ app/Post.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+
+class Post extends Model
+{
+    protected $fillable = ['title', 'description', 'cost', 'slug', 'user_id', 'category_id' ];
+    protected $table = "post";
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+    public function category()
+    {
+        return $this->hasOne(Categories::class, 'id', 'category_id');
+    }
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function addComment($body) {
+
+        $this->comments()->create(compact('body'));
+
+    }
+}
