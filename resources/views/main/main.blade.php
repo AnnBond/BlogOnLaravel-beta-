fbbc6577502c6927004f8e3d26eaf017d638abbd
AnnBond <annbondar4uk@gmail.com>
2017-09-24 01:23:40 +0300

    improve href add sidebar

--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -1,24 +1,32 @@
 @extends('default_layout')
 
 @section('content')
-    <div class="wrapper">
-        <div class="row">
+    <div class="row">
+    <div class="col-lg-9">
+            <p class="float-right d-md-none">
+                <button type="button" class="btn btn-primary btn-sm" data-toggle="offcanvas">Toggle nav</button>
+            </p>
+            <div class="jumbotron">
+                <h1>Hello, world!</h1>
+                <p>This is an example to show the potential of an offcanvas layout pattern in Bootstrap. Try some responsive-range viewport sizes to see it in action.</p>
+            </div>
+            <div class="row">
             <?php foreach ($posts as $post): ?>
-            <div class="card col-6" >
-                <div class="card-body">
-                    <p class="date">{{ $post->created_at }}</p>
-                    <h4 class="card-title"><a href="{{ route('singlePost', ['id'=> $post->id]) }}" class="card-title">{{ $post->title }}</a></h4>
+                <div class="col-6 col-lg-4" style="padding: 50px 0;">
+                    <p class="date">{{ $post->created_at->toFormattedDateString() }}</p>
+                    <h4 class="card-title"><a href="/posts/{{ $post->id }}" class="card-title">{{ $post->title }}</a></h4>
                     <p class="cost">{{ $post->cost }}</p>
                     <p class="card-text">Author: {{ $post->author->name }}</p>
-                    <p class="cost">Category: <a href="{{ route('categoryById', ['id'=> $post->category_id]) }}" class="card-title">{{
+                    <p class="category">Category: <a href="/category/{{ $post->category_id }}" class="card-title">{{
                         $post->category->name
                         }}</a></p>
                     <p class="card-text">{{ $post->description }}</p>
-                    <a href="{{ route('singlePost', ['id'=> $post->id]) }}" class="btn btn-primary">See more</a>
+                    <a href="/posts/{{ $post->id }}" class="btn btn-primary">See more</a>
                 </div>
-            </div> <br>
+                <br>
             <?php endforeach; ?>
-        </div>
-        <br>
+            </div>
+    </div>
+    @include('main.sidebar')
     </div>
     @endsection
