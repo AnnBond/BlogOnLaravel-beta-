9121cd4af2ee72259aa4405eaf25006d4b2ea9dd
AnnBond <annbondar4uk@gmail.com>
2017-09-24 01:17:21 +0300

    create post page

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,25 @@
+@extends('default_layout')
+
+@section('content')
+    <h1>Create a post</h1>
+    <form method="POST" action="/posts">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" class="form-control" id="title" name="title">
+        </div>
+        <div class="form-group">
+            <label for="description">Descriotion:</label>
+            <textarea class="form-control"  name="description" id="description" cols="30" rows="10"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="cost">Cost</label>
+            <input type="number" class="form-control" id="cost" placeholder="200" name="cost">
+        </div>
+        @include('layouts.errors')
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">Publish</button>
+        </div>
+    </form>
+
+@endsection
