36f97f46da96ff9fd384766bd6ed661f577c7242
AnnBond <annbondar4uk@gmail.com>
2017-09-24 01:16:15 +0300

    about.page

---
+++ resources/views/main/about.blade.php
@@ -0,0 +1,13 @@
+@extends('default_layout')
+
+@section('content')
+        <div class="row">
+            <h1>About us</h1>
+            <div class="about-block">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam animi aspernatur enim error est et ex expedita, id ipsam iure molestias nesciunt omnis quibusdam, repellendus soluta, voluptatem voluptatibus. A, delectus?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam animi aspernatur enim error est et ex expedita, id ipsam iure molestias nesciunt omnis quibusdam, repellendus soluta, voluptatem voluptatibus. A, delectus?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam animi aspernatur enim error est et ex expedita, id ipsam iure molestias nesciunt omnis quibusdam, repellendus soluta, voluptatem voluptatibus. A, delectus?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam animi aspernatur enim error est et ex expedita, id ipsam iure molestias nesciunt omnis quibusdam, repellendus soluta, voluptatem voluptatibus. A, delectus?
+            </div>
+        </div>
+@endsection
