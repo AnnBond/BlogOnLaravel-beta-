8f9f4c00db6cf2e8153ef83ecc2d740ed332c84f
AnnBond <annbondar4uk@gmail.com>
2017-09-24 01:26:06 +0300

    store post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,14 +3,85 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Posts;
+use App\Post;
 
 class PostsController extends Controller
 {
-    public function singlePost (Posts $post) {
+    public function index () {
+
+    }
+
+    public function create() {
+
+        return view('posts.create');
+    }
+
+    public function store()
+    {
+        $this->validate(request(), [
+           'title' => 'required',
+            'description' => 'required',
+            'cost' => 'required'
+        ]);
+        //dd(request(['title', 'description']));
+        $post = new Post;
+
+        Post::create([
+           'title' => request('title'),
+            'slug' => request('title'),
+            'user_id' => '5',
+            'category_id' => '5',
+            'description' => request('description'),
+            'cost' => request('cost')
+        ]);
+
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post) {
 
         /*$post = Posts::with('author', 'category')->find($id);*/
 
         return view('posts.singlePost', compact('post'));
     }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
 }
