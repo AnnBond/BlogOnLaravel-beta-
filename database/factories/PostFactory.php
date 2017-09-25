a99199e0698410d904cdc486b1096ad0c69cc75b
AnnBond <annbondar4uk@gmail.com>
2017-09-25 23:22:28 +0300

    small fix

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -9,7 +9,7 @@ $factory->define(App\Post::class, function (Faker $faker) {
 
     return [
     'title' => $faker->text(40),
-    'slug' => $faker->text(40),
+    /*'slug' => $faker->text(40),*/
     'description' => $faker->text(200),
     'cost' =>$faker->biasedNumberBetween(100, 2000),
     'user_id' => User::all()->random()->id,
