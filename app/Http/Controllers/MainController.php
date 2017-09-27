877437df80d54e36b9868280c39e859923a90b95
AnnBond <annbondar4uk@gmail.com>
2017-09-27 22:32:11 +0300

    add categories

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\Categories;
 
 class MainController extends Controller
 {
     public function index() {
         $posts = Post::latest()->with('author', 'category')->get();
 
-        return view('main.main', compact('posts'));
+        $categories = Categories::all();
+
+
+        return view('main.main', compact('posts', 'categories'));
     }
 
     public function about() {
