253c5739378e259dba133e5349e0d2fe89ba2aec
AnnBond <annbondar4uk@gmail.com>
2017-09-27 19:46:50 +0300

    add list of categories

--- resources/views/main/sidebar.blade.php
+++ resources/views/main/sidebar.blade.php
@@ -1,14 +1,15 @@
-<div class="col-lg-3 sidebar-offcanvas" id="sidebar">
-    <div class="list-group">
-        <a href="#" class="list-group-item active">Link</a>
-        <a href="#" class="list-group-item">Link</a>
-        <a href="#" class="list-group-item">Link</a>
-        <a href="#" class="list-group-item">Link</a>
-        <a href="#" class="list-group-item">Link</a>
-        <a href="#" class="list-group-item">Link</a>
-        <a href="#" class="list-group-item">Link</a>
-        <a href="#" class="list-group-item">Link</a>
-        <a href="#" class="list-group-item">Link</a>
-        <a href="#" class="list-group-item">Link</a>
+<div class="col-lg-3">
+    <div class="allCategories">
+        <h2 style="text-align: center">Categories</h2>
+        <br>
+        <ul style="list-style: none;">
+        @foreach($categories as $category)
+            <li style="font-size: 25px">
+                <a href="{{ route('categoryById', ['id'=> $category->id]) }}" class="card-title">{{ $category->name }}</a>
+            </li>
+                <hr>
+        @endforeach
+        </ul>
     </div>
 </div>
+
